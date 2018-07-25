3d9721cd9ad6586d8fd77ad0ee10370a9f6c1dc9
Leo <kl@alushta.net>
2018-07-25 22:00:35 +0300

    Adding colorpicker in visit handler

--- app.rb
+++ app.rb
@@ -19,12 +19,13 @@ post '/visit' do
 	@barber = params[:barber]
 	@username = params[:username]
 	@date = params[:date]
+	@color = params[:colorpicker]
 
 	f = File.open './public/file.txt', 'a'
-	f.write "Barber is: #{@barber}, Username: #{@username}, date: #{@date}"
+	f.write "Barber is: #{@barber}, Username: #{@username}, date: #{@date}, color: #{@color}"
 	f.close
 
-	erb "Dear #{@username}! We will wait for you on #{@date}. Remember, your barber is #{@barber}"
+	erb "Dear #{@username}! We will wait for you on #{@date}. Remember, your barber is #{@barber} and color is #{@color}"
 end
 
 
@@ -37,7 +38,7 @@ post '/contacts' do
 	@textarea = params[:textarea]
 
 	f = File.open './public/contacts.txt', 'a'
-	f.write "Email: #{@email}, message: #{@textarea}"
+	f.write "===Email: #{@email}, message: #{@textarea}===/r/n"
 	f.close
 
 	erb 'Thank you for your message. We will contact you as soon as possible.'
